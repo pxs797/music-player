ad94de12db56f68e12c551f76dfb9a839d0f58a9
mark <1210917553@qq.com>
2022-10-12 11:46:03 +0800

    feat: update get token api

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
+<script setup>
+import { onMounted } from 'vue'
+import { getToken } from './utils/auth.js'
+import Application from '@/layouts/Application.vue'
+
+onMounted(async () => {
+  // const { access_token: token} = await getToken()
+  // localStorage.setItem('token', token)
+  // console.log(token);
+})
+
+</script>
+
 <template>
-  <div class="w-screen h-screen bg-blue-900">
-    <router-view></router-view>
-  </div>
+  <application>
+    <router-view />
+  </application>
 </template>
