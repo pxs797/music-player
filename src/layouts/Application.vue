c69c10d9d903e0e47986a5ee82af3c31c3b69ac1
mark <1210917553@qq.com>
2022-11-07 13:33:55 +0800

    chore: update layout

--- src/layouts/Application.vue
+++ src/layouts/Application.vue
@@ -1,13 +1,15 @@
 <script setup>
 import SideBar from '@/components/SideBar.vue'
 import Playground from '@/components/Playground.vue'
+import Player from '@/components/Player.vue'
 </script>
 
 <template>
-  <div class="w-screen h-screen overflow-hidden flex">
+  <div class="w-screen h-screen overflow-hidden flex relative">
     <side-bar></side-bar>
     <playground>
       <slot></slot>
     </playground>
+    <player></player>
   </div>
 </template>
